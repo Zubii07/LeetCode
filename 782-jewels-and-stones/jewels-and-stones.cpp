4c0b99aee894a5e36b8fcf1bc96ccae780290ea8
M Zohaib <167600779+Zubii07@users.noreply.github.com>
2024-05-27 15:07:55 +0500

    Time: 0 ms (100.00%) | Memory: 7.5 MB (92.58%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n=0;
+        for(int i=0;i<stones.size();++i){
+            char c=stones[i];
+            if(jewels.find(c) !=-1){
+                n++;
+            }
+        }
+        return n;
+    }
+};
